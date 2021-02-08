88d9cb1aa85071421cd5458f3f8a4de2f9a9a511
AmadoJunior <amadoelrepalol@gmail.com>
2021-02-08 13:56:39 -0500

    making api http

--- src/main/java/com/myWebsite/SPA/Configurations/WebSecurityConfig.java
+++ src/main/java/com/myWebsite/SPA/Configurations/WebSecurityConfig.java
@@ -2,10 +2,16 @@ package com.myWebsite.SPA.Configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/api/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
