41db76d2e3f988390383d15fa1cea467473dde22
AmadoJunior <amadoelrepalol@gmail.com>
2021-02-15 02:32:09 -0500

    service worker now listens for fetch events and return cached data first

--- src/main/resources/static/serviceWorker.js
+++ src/main/resources/static/serviceWorker.js
@@ -37,18 +37,40 @@ this.addEventListener("install", (event) => {
     )
 })
 
-//Listen for Requests
-// this.addEventListener("fetch", (event) => {
-//     event.respondWith(
-//         caches.match(event.request)
-//             .then(() => {
-//                 return fetch(event.request)
-//                     .catch(() => {
-//                         return caches.match("offline.html")
-//                     })
-//             })
-//     )
-// })
+//Listen to Fetch Events
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
 
 //Activate SW
 this.addEventListener("activate", (event) => {
